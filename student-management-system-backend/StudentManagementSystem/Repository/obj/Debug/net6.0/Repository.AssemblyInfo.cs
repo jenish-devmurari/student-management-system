5bf5c1bc3184bcda7d9b08e56f8a0699cf468317
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-06-11 22:35:02 +0530

    add .gitignore file

--- student-management-system-backend/StudentManagementSystem/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ student-management-system-backend/StudentManagementSystem/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5476720d93baca80927a888daf83e8de061cffb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f214a52b45d3b1e0a57c895da85265bedf239f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
