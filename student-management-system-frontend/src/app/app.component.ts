369b199657f2ab381e8b3f4041b4509422b2e6a2
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-06-17 11:31:00 +0530

    install bootstrap,ngx-toaster,sweet-alert and create modules for application

--- student-management-system-frontend/src/app/app.component.ts
+++ student-management-system-frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'student-management-system-frontend';
+export class AppComponent implements OnInit {
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.toastr.success("welcome ")
+  }
+
+  showAlert() {
+    Swal.fire({
+      title: 'Success!',
+      text: 'SweetAlert2 is working',
+      icon: 'success',
+      confirmButtonText: 'Cool'
+    });
+  }
+
 }
--- student-management-system-frontend/src/app/app.module.ts
+++ student-management-system-frontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      closeButton: true,
+      progressBar: true,
+      positionClass: 'toast-top-right',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
